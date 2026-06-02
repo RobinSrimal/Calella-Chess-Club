07fddc23762cb50eb3c34b5cda11840b09c78b47
Robin Srimal <robinsrimal@iMac.local>
2026-06-02 18:32:32 +0200

    Configure Cloudflare provider for SST

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,4 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -7,12 +6,12 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: { cloudflare: "6.17.0" },
     };
   },
   async run() {
     const storage = await import("./infra/storage");
     await import("./infra/api");
-
     return {
       MyBucket: storage.bucket.name,
     };
